62b92851425fbe152f0ff589a4f1f63847b5680b
Gurpreet Singh <gurpreet_singh_hanjra@mckinsey.com>
2016-09-07 15:49:40 +0530

    reached till edit profile page

--- test.js
+++ test.js
@@ -1,15 +1,38 @@
 var webdriverio = require('webdriverio');
 var options = {
     desiredCapabilities: {
-        browserName: 'chrome'
+        browserName: 'firefox'
     }
 };
+
+var currentWeekIndex = 0;
+
+var url = 'https://mckinsey.moveinsync.com/Ncr/',
+    username = 'gurpreet_singh_hanjra@mckinsey.com',
+    password = 'gyrml@143';
 
 webdriverio
     .remote(options)
     .init()
-    .url('http://www.google.com')
+    .url(url)
+    .setValue('#username', username)
+    .setValue("[name='j_password']", password)
+    .click('.buttonstyle1')
+    .waitForExist('.modifySchedule', 5000)
     .getTitle().then(function(title) {
         console.log('Title was: ' + title);
     })
-    .end();
+    .isExisting('#login').then(function(isExisting) {
+        console.log(isExisting); // outputs: false
+    })
+    /*.click("[name='weekList']")
+    .getValue("[name='weekList']")
+    .then(function(value) {
+        currentWeekIndex = Number(value) + 1;
+        console.log(this, value, currentWeekIndex);
+
+    })*/
+    .click("[name='weekList'] option[value='2']")
+    //.selectByIndex("[name='weekList']", currentWeekIndex)
+    //.click('#login')
+    ///.end();
